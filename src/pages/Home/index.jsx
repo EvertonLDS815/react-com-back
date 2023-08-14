f0ceacf605daa228afa3c5cda14c3f28d9988d2d
EvertonLDS815 <evertonlino1234@gmail.com>
2023-08-14 09:16:48 -0300

    feat: add redirects

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,11 @@
+
+function Home() {
+    return (
+        <>
+            <h1>Página Home</h1>
+            <a href="/products">Products</a>
+        </>
+    );
+}
+
+export default Home;
--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -2,13 +2,15 @@ import { Route, Routes } from "react-router-dom";
 
 import DefaultLayout from "../layout/DefaultLayout";
 import Products from "../pages/Products";
+import Home from "../pages/Home";
 
 function router() {
   return (
     <>
       <Routes>
         <Route element={<DefaultLayout />}>
-          <Route path="/" element={<Products />}></Route>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/products" element={<Products />}></Route>
         </Route>
       </Routes>
     </>
